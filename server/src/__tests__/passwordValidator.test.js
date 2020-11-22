f8dfe1347df38310fdaa0567609d825ab4d5c76d
Łukasz Basaj <lbasaj@gmail.com>
2020-11-22 14:01:58 +0100

    add password validator and tests

---
+++ server/src/__tests__/passwordValidator.test.js
@@ -0,0 +1,32 @@
+const { passwordValidator } = require('../auth/passwordValidator');
+
+describe.only('passwordValidator', () => {
+    // test('returns true for a valid password', () => {
+    //     const isValid =  passwordValidator('lksamdo32');
+    //     expect(isValid).toBe(true);
+    // });
+    test('returns false if password is shorter than 6 characters', () => {
+        const isValid =  passwordValidator('lksam');
+        expect(isValid).toBe(false);
+    });
+
+    test('returns false if password not containinig non-alphanumerical char', () => {
+        const isValid =  passwordValidator('asdasd444');
+        expect(isValid).toBe(false);
+    });
+
+    test('returns false if password not containing a digit', () => {
+        const isValid =  passwordValidator('asdasd@');
+        expect(isValid).toBe(false);
+    });
+
+    test('returns true for valid password', () => {
+        const isValid =  passwordValidator('asdasd@1A');
+        expect(isValid).toBe(true);
+    });
+
+    test('returns false if password not containing a capital letter', () => {
+        const isValid =  passwordValidator('asdasd@1');
+        expect(isValid).toBe(false);
+    })
+});
